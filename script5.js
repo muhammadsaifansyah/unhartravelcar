1331509929186d95d4856c9974d49b98ba7827dc
UnharTravelCar <91236077+muhammadsaifansyah@users.noreply.github.com>
2023-05-31 21:41:49 -0700

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,26 @@
+// Toggle class active
+const navbarNav = document.querySelector ('.navbar-Nav');
+// ketika di klik 
+document.querySelector('#hamburger-menu'). onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+
+
+// klik diluar sidebar
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+ if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  navbarNav.classList.remove('active');
+ }
+});
+
+// perkalian //
+function add(){
+var a,b,c; 
+a=Number(220000);
+b=Number(document.getElementById("second").value); 
+c = a * b; 
+document.getElementById("answer").value= c;
+
+}
